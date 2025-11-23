fdfd4e770739dd684167ae76e06f96a324f0f4eb
Frans16 <franstmarbun@gmail.com>
2025-11-23 23:45:11 +0700

    update experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,25 +6,25 @@ const Experience = () => {
     {
       year: '2024 - Present',
       title: 'QA Specialist (Lead)',
-      company: 'Digital Agency',
+      company: 'Antikode - Digital Agency',
       description: 'Responsible for defining and managing the entire Quality Assurance (QA) strategy for digital projects, ensuring that all websites, applications, and campaigns are functionally sound and meet high quality standards before launch. I personally lead the testing process and serve as the critical bridge between the development team and the client\'s expectations, acting as the final quality authority.'
     },
     {
       year: '2022 - 2024',
       title: 'QA Engineer',
-      company: 'HRIS',
+      company: 'Appsensi - HRIS',
       description: 'Led the QA team in a high-profile project, ensuring the successful implementation of a robust Human Resources Information System (HRIS) solution. Collaborated closely with cross-functional teams to identify and address potential issues, resulting in a 20% increase in employee satisfaction. Implemented a comprehensive testing strategy, including unit tests, integration tests, and end-to-end tests, to validate the functionality and performance of the system. Actively participated in the development process, providing feedback and suggestions to drive continuous improvement.'
     },
     {
       year: '2020 - 2022',
       title: 'QA Engineer',
-      company: 'Insurance',
+      company: 'Prudential Indonesia - Insurance',
       description: 'Led the QA team in a high-profile project, ensuring the successful implementation of a robust Insurance Information System (INS) solution. Collaborated closely with cross-functional teams to identify and address potential issues, resulting in a 20% increase in customer satisfaction. Implemented a comprehensive testing strategy, including unit tests, integration tests, and end-to-end tests, to validate the functionality and performance of the system. Actively participated in the development process, providing feedback and suggestions to drive continuous improvement.'
     },
     {
       year: '2018 - 2020',
       title: 'Manual QA',
-      company: 'Outsourcing',
+      company: 'IDStar - Outsourcing',
       description: 'Worked as a Manual QA Engineer for an outsourcing company, responsible for testing and validating software applications. Collaborated with cross-functional teams to identify and address potential issues, ensuring the quality and functionality of the software. Conducted thorough testing activities, including functional testing, regression testing, and usability testing, to meet the client\'s requirements. Provided detailed bug reports and documentation, facilitating effective communication with the development team. Actively participated in the software development lifecycle, contributing to the delivery of high-quality software products.'
     }
   ];
